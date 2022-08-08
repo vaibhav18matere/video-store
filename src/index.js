7e4f3348f293a2ba2b9c7f9676d0f60c71dd2d34
Vaibhav Matere <vpm00718897@gmail.com>
2022-08-08 17:42:36 +0530

    setup API key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/pages/Trending/Trending.js
+++ src/pages/Trending/Trending.js
@@ -1,7 +1,30 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Trending = () => {
-  return <div>Trending</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchTrendingata();
+  });
+
+  const fetchTrendingata = async () => {
+    const { responseData } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
+    );
+
+    console.log(responseData);
+    setMovies(responseData.results);
+  };
+  fetchTrendingata();
+
+
+  return (
+    <>
+      <div>Trending</div>
+    </>
+  );
 };
 
 export default Trending;
