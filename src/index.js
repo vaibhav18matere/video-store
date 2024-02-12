088e8a6e3813569be046f8f7674491ec9494255f
Vaibhav Matere <vpm00718897@gmail.com>
2024-02-12 20:58:05 +0530

    database connection error handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({ path: './env' });
 
-connectDB();
+connectDB()
+     .then(() => {
+
+          app.on('error', (err) => {
+               console.log('error is...', err);
+               throw err
+          })
+
+          app.listen(process.env.PORT || 8000, () => {
+               console.log(`server is running at PORT: ${process.env.PORT}`);
+          })
+     })
+     .catch((err) => {
+          console.log(err);
+     })
